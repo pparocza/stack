83557a4962a82a65bdc5c92e5e662ab27ae6b3b2
paparoc <paulparoczai@gmail.com>
2021-10-04 12:00:26 -0700

    structure methods

--- scripts/parts.js
+++ scripts/parts.js
@@ -9,7 +9,7 @@ class Piece {
         this.globalNow = 0;
 
         this.gain = audioCtx.createGain();
-        this.gain.gain.value = 2;
+        this.gain.gain.value = 1.5;
     
         this.fadeFilter = new FilterFade(0);
     
@@ -24,89 +24,106 @@ class Piece {
 
         // REVERB
 
-        this.c = new MyConvolver();
-        this.cB = new MyBuffer2( 2 , 2 , audioCtx.sampleRate );
-        this.cB.noise().fill( 0 );
-        this.cB.noise().fill( 1 );
-        this.cB.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 0 );
-        this.cB.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 1 );
+            this.cSend = new MyGain( 1 );
 
-        this.c.setBuffer( this.cB.buffer );
+            this.c = new MyConvolver();
+            this.cB = new MyBuffer2( 2 , 2 , audioCtx.sampleRate );
+            this.cB.noise().fill( 0 );
+            this.cB.noise().fill( 1 );
+            this.cB.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 0 );
+            this.cB.ramp( 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ).multiply( 1 );
+
+            this.c.setBuffer( this.cB.buffer );
+
+            this.cSend.connect( this.c );
+            this.c.connect( this.masterGain );
+
+        // DELAY
+
+            this.dSend = new MyGain( 1 );
+            this.d = new Effect();
+            this.d.randomEcho();
+            this.d.on();
+
+            this.d.output.gain.value = 0.25;
 
     }
 
     load(){
 
-                // RAMPING CONVOLVER
+            this.rC5 = new RampingConvolver( this );
+            this.rC4 = new RampingConvolver( this );
+            this.rC3 = new RampingConvolver( this );
+            this.rC2 = new RampingConvolver( this );
+            this.rC1 = new RampingConvolver( this );
 
-                this.fund = randomFloat( 350 , 450 );
-                this.rate = 4;
-                this.rateMult = randomFloat( 1 , 1 );
-        
-                // startTime , rate , rampArray , bufferLength , fund , frequencyRange , gainVal
-                this.rC5 = new RampingConvolver( this.rateMult * ( this.rate / 3 )  , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 0.5 , 1 , 2 ])        , this.fund * 0.5 , [ 100 , 5000 ]  , 2 );
-                this.rC4 = new RampingConvolver( this.rateMult * ( this.rate * 1 )  , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 0.25 , 0.5 , 1 , 2 ]) , this.fund       , [ 100 , 1000 ]  , 2 );
-                this.rC3 = new RampingConvolver( this.rateMult * ( this.rate * 2 )  , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 0.25 , 0.5 , 1 , 2 ]) , this.fund       , [ 4000 , 7000 ] , 2 );
-                this.rC2 = new RampingConvolver( this.rateMult * ( this.rate / 16 ) , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 1 ])                  , this.fund       , [ 100 , 500 ]   , 2 );
-                this.rC1 = new RampingConvolver( this.rateMult * ( this.rate / 32 ) , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 2 ])                  , this.fund * 0.5 , [ 100 , 500 ]   , 2 );
-        
-                this.rC5A = new RampingConvolver( this.rateMult * ( this.rate / 3 )  , [ 0 , 1 , 0.5 , 0.5 , 1 , 1 ]      , 2 ,  this.fund * 0.5 , [ 100 , 5000 ] , 0.5 );
-                this.rC4A = new RampingConvolver( this.rateMult * ( this.rate * 1 )  , [ 0 , 1 , 0.5 , 0.5 , 1 , 1 ]      , 2 ,  this.fund       , [ 100 , 1000 ] , 0.5 );
-                this.rC3A = new RampingConvolver( this.rateMult * ( this.rate / 16 ) , [ 0 , 1 , 0.99 , 0.999 , 5 , 0.5 ] , 2 ,  this.fund * 0.5 , [ 100 , 5000 ] , 1   );
-                this.rC2A = new RampingConvolver( this.rateMult * ( this.rate / 8)   , [ 0 , 1 , 0.99 , 0.999 , 5 , 0.5 ] , 2 ,  this.fund       , [ 100 , 1000 ] , 1   );
-        
-        
-                this.rC1.output.connect( this.masterGain );
-                this.rC1.output.connect( this.c );
-        
-                this.rC2.output.connect( this.masterGain );
-                this.rC2.output.connect( this.c );
-        
-                this.rC3.output.connect( this.masterGain );
-                this.rC3.output.connect( this.c );
-                
-                this.rC4.output.connect( this.masterGain );
-                this.rC4.output.connect( this.c );
-        
-                this.rC5.output.connect( this.masterGain );
-                this.rC5.output.connect( this.c );
-        
-                this.rC4A.output.connect( this.masterGain );
-                this.rC4A.output.connect( this.c );
-        
-                this.rC5A.output.connect( this.masterGain );
-                this.rC5A.output.connect( this.c );
-        
-                this.rC2A.output.connect( this.masterGain );
-                this.rC2A.output.connect( this.c );
-        
-                this.rC3A.output.connect( this.masterGain );
-                this.rC3A.output.connect( this.c );
-        
-                this.c.connect( this.masterGain );
+            this.rC5A = new RampingConvolver( this );
+            this.rC4A = new RampingConvolver( this );
+            this.rC3A = new RampingConvolver( this );
+            this.rC2A = new RampingConvolver( this );
 
-                this.rCArray = [ this.rC1 , this.rC2 , this.rC3 , this.rC4 , this.rC5 , this.rC4A , this.rC5A , this.rC2A , this.rC3A ];
+            // RAMPING CONVOLVER
 
-                this.qN = 4 * ( 1 / this.rate );
-                this.bar = 4 * this.qN;
-                this.nBars = 16;
+            this.fund = randomFloat( 350 , 450 );
+            this.rate = 4;
+            this.rateMult = randomFloat( 1 , 1 );
+    
+            // startTime , rate , rampArray , bufferLength , fund , frequencyRange , gainVal
 
-                this.structureArray = [];
+                this.rC5.load( this.rateMult * ( this.rate / 3 )  , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 0.5 , 1 , 2 ])        , this.fund * 0.5 , [ 100 , 5000 ]  , 2 );
+                this.rC4.load( this.rateMult * ( this.rate * 1 )  , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 0.25 , 0.5 , 1 , 2 ]) , this.fund       , [ 100 , 1000 ]  , 2 );
+                this.rC3.load( this.rateMult * ( this.rate * 2 )  , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 0.25 , 0.5 , 1 , 2 ]) , this.fund       , [ 4000 , 7000 ] , 2 );
+                this.rC2.load( this.rateMult * ( this.rate / 16 ) , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 1 ])                  , this.fund       , [ 100 , 500 ]   , 2 );
+                this.rC1.load( this.rateMult * ( this.rate / 32 ) , [ 0 , 1 , 0.01 , 0.015 , 0.1 , 4 ] , randomArrayValue( [ 2 ])                  , this.fund * 0.5 , [ 100 , 500 ]   , 2 );
+        
+                this.rC5A.load( this.rateMult * ( this.rate / 3 )  , [ 0 , 1 , 0.5 , 0.5 , 1 , 1 ]      , 2 ,  this.fund * 0.5 , [ 100 , 5000 ] , 0.5 );
+                this.rC4A.load( this.rateMult * ( this.rate * 1 )  , [ 0 , 1 , 0.5 , 0.5 , 1 , 1 ]      , 2 ,  this.fund       , [ 100 , 1000 ] , 0.5 );
+                this.rC3A.load( this.rateMult * ( this.rate / 16 ) , [ 0 , 1 , 0.99 , 0.999 , 5 , 0.5 ] , 2 ,  this.fund * 0.5 , [ 100 , 5000 ] , 1   );
+                this.rC2A.load( this.rateMult * ( this.rate / 8)   , [ 0 , 1 , 0.99 , 0.999 , 5 , 0.5 ] , 2 ,  this.fund       , [ 100 , 1000 ] , 1   );
 
-                for( let i = 0 ; i < this.nBars ; i++ ){
+            this.rCArray = [ this.rC1 , this.rC2 , this.rC3 , this.rC4 , this.rC5 , this.rC4A , this.rC5A , this.rC2A , this.rC3A ];
 
-                    this.structureArray[ i ] = [];
+            this.qN = 4 * ( 1 / this.rate );
+            this.bar = 4 * this.qN;
+            this.nBars = 16;
 
-                    for( let j = 0 ; j < this.rCArray.length - 1 ; j++ ){
+            this.generateStructure();
 
-                        this.structureArray[i].push( randomInt( 0 , 2 ) );
+    }
 
-                    }
+    generateStructure(){
 
-                    this.structureArray[i].push( 1 );
-                    shuffle( this.structureArray[i] );
-                    
-                }
+        this.randomStructure( 3 );
+
+    }
+
+    randomStructure( minimumVoices ){
+
+        this.structureArray = [];
+
+        for( let i = 0 ; i < this.nBars ; i++ ){
+
+            this.structureArray[ i ] = [];
+
+            for( let j = 0 ; j < this.rCArray.length - minimumVoices ; j++ ){
+
+                this.structureArray[i].push( randomInt( 0 , 2 ) );
+
+            }
+
+            for( let j = 0 ; j < minimumVoices ; j++ ){
+
+                console.log( j );
+
+                this.structureArray[i].push( 1 );
+
+            }
+
+            shuffle( this.structureArray[i] );
+            
+        }
+
+        console.log( this.structureArray );
 
     }
 
@@ -144,9 +161,19 @@ class Piece {
 
 class RampingConvolver{
 
-    constructor( rate , rampArray , bufferLength , fund , frequencyRange , gainVal ){
+    constructor( piece ){
 
-        this.output = new MyGain( gainVal );
+        this.output = new MyGain( 0 );
+
+        this.output.connect( piece.masterGain );
+        this.output.connect( piece.cSend );
+        this.output.connect( piece.dSend );
+
+    }
+
+    load( rate , rampArray , bufferLength , fund , frequencyRange , gainVal ){
+
+        this.output.gain.gain.value = gainVal;
 
         this.c = new MyConvolver();
         this.cB = new MyBuffer2(  1 , bufferLength , audioCtx.sampleRate );
